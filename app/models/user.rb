9a093d84db86ca8a339b3d975471854b4916a222
girigiri-s <83535149+girigiri-s@users.noreply.github.com>
2021-09-05 19:55:00 +0900

    モデルの単体テストの実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,12 +5,13 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   validates :nickname,           presence: true
-  validates :email,              presence: true, uniqueness: true
+  validates :email,              presence: true, uniqueness: true, format: { with: /@.+/, message: 'には＠を含めて設定してください' }
+  validates :password,           presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z{6,}/i, message: 'には英字と数字の両方を含めて設定してください'}
   validates :encrypted_password, presence: true
-  validates :last_name,          presence: true
-  validates :first_name,         presence: true
-  validates :last_name_kana,     presence: true
-  validates :first_name_kana,    presence: true
+  validates :last_name,          presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'には全角文字を使用してください'}
+  validates :first_name,         presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'には全角文字を使用してください'}
+  validates :last_name_kana,     presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'には全角カタカナを使用してください'}
+  validates :first_name_kana,    presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'には全角カタカナを使用してください'}
   validates :birthday,           presence: true
 
 end
