96440cbda7be11ce639d7edcb122cb4c54fea2da
girigiri-s <83535149+girigiri-s@users.noreply.github.com>
2021-09-06 22:29:25 +0900

    修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,9 +5,7 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   
   validates :nickname,           presence: true
-  validates :email,              presence: true, uniqueness: true, format: { with: /@.+/, message: 'には＠を含めて設定してください' }
-  validates :password,           presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z{6,}/i, message: 'には英字と数字の両方を含めて設定してください'}
-  validates :encrypted_password, presence: true
+  validates :password,           presence: true, format: { with: /\A(?=.*?[a-z])(?=.*?[\d])[a-z\d]+\z{6,}/i, message: 'には半角英字と半角数字の両方を含めて設定してください'}
   validates :last_name,          presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'には全角文字を使用してください'}
   validates :first_name,         presence: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: 'には全角文字を使用してください'}
   validates :last_name_kana,     presence: true, format: { with: /\A[ァ-ヶー]+\z/, message: 'には全角カタカナを使用してください'}
